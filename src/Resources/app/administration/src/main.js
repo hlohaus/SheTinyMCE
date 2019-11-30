0ebacfd79bb437b360e64b7791ac7f7f6d7dc46b
Heiner Lohaus <h.lohaus@she-business.com>
2019-11-30 02:43:15 +0100

    first commit

---
+++ src/Resources/app/administration/src/main.js
@@ -0,0 +1,95 @@
+import { Component } from 'src/core/shopware';
+import template from './extension/sw-tinymce.html.twig';
+import './sw-tinymce.scss';
+
+Component.override('sw-text-editor', {
+    template,
+
+    data() {
+        return {
+            mediaModalIsOpen: false
+        };
+    },
+
+    methods: {
+        onCloseMediaModal() {
+            this.mediaModalIsOpen = false;
+        },
+
+        onMediaSelectionChange(mediaItems) {
+            mediaItems.forEach((item) => {
+                this.filePickerCallback(item.url);
+            });
+        },
+
+        onChange(e) {
+            this.emitHtmlContent(e.target.getBody().innerHTML);
+        }
+    },
+
+    mounted() {
+        if (this.isInlineEdit) {
+            this.mountedComponent();
+            return;
+        }
+        const me = this;
+        const lang = window.localStorage.getItem('sw-admin-locale') || 'en';
+        window.tinymce.init({
+            target: this.$refs.textArea,
+            language: lang.substring(0, 2),
+            height: 300,
+            plugins: 'print preview paste importcss searchreplace autolink' +
+            ' autosave save directionality code visualblocks visualchars' +
+            ' fullscreen image link media template codesample table charmap' +
+            ' hr pagebreak nonbreaking anchor toc insertdatetime advlist' +
+            ' lists wordcount imagetools textpattern noneditable help charmap' +
+            ' quickbars emoticons',
+            menubar: 'file edit view insert format tools table help',
+            toolbar: 'undo redo | bold italic underline strikethrough |' +
+            ' fontselect fontsizeselect formatselect |' +
+            ' alignleft aligncenter alignright alignjustify |' +
+            ' outdent indent |  numlist bullist |' +
+            ' forecolor backcolor removeformat | pagebreak |' +
+            ' charmap emoticons | fullscreen  preview save print |' +
+            ' insertfile image media template link anchor codesample |' +
+            ' ltr rtl',
+            toolbar_sticky: true,
+            image_advtab: true,
+            content_css: [],
+            link_list: [
+                { title: 'My page 1', value: 'http://www.tinymce.com' },
+                { title: 'My page 2', value: 'http://www.moxiecode.com' }
+            ],
+            image_list: [
+                { title: 'My page 1', value: 'http://www.tinymce.com' },
+                { title: 'My page 2', value: 'http://www.moxiecode.com' }
+            ],
+            image_class_list: [
+                { title: 'None', value: '' },
+                { title: 'Some class', value: 'class-name' }
+            ],
+            importcss_append: true,
+            file_picker_callback: function (callback) {
+                /* Provide file and text for the link dialog */
+                me.mediaModalIsOpen = true;
+                me.filePickerCallback = callback;
+            },
+            templates: [
+                { title: 'New Table', description: 'creates a new table', content: '' },
+                { title: 'Starting my story', description: 'A cure for writers block', content: 'Once upon a time...' }
+            ],
+            template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
+            template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
+            image_caption: true,
+            quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+            noneditable_noneditable_class: 'mceNonEditable',
+            toolbar_drawer: 'sliding',
+            contextmenu: 'link image imagetools table',
+            init_instance_callback: function (editor) {
+                editor.on('Change', me.onChange);
+            },
+            extended_valid_elements: 'script[src|async|defer|type|charset]'
+        });
+    }
+});
+
---
+++ src/SheTinyMCE.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace She\TinyMCE;
+
+use Shopware\Core\Framework\Plugin;
+
+class SheTinyMCE extends Plugin
+{
+}
